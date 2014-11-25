7ed5919d57672632fea082356ecd8d39952b726d
Ethan Houston <ethan.houston@gmail.com>
2014-11-25 21:39:51 +0800

    Create number 9, number 10

---
+++ src/number10.java
@@ -0,0 +1,15 @@
+public class number10 {
+    public static void main(String[] args){
+        
+    }
+    public static void running(){
+        
+    }
+    public static boolean primeChecker(int n){
+        for(int i=2;i<n;i++) {
+        if(n%i==0)
+            return false;
+    }
+    return true;
+    }
+}

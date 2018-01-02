d64cdb3ed84fa97deca0c5247f8bfcf6a894d34e
ethanzh <ethan.houston@gmail.com>
2018-01-01 18:56:48 -0800

    Finished problem 1

--- src/problem1.java
+++ src/problem1.java
@@ -1,2 +1,27 @@
 public class problem1 {
+
+    public static void main(String[] args){
+
+        int top = 1000;
+
+        int total = 0;
+
+        for(int i = 1; i < top; i++){
+
+            if (divisible(i, 3) || divisible(i, 5)){
+                total += i;
+            }
+
+        }
+
+        System.out.println(total);
+
+    }
+
+    static boolean divisible(int number, int divisor){
+
+
+        return number % divisor == 0; // Simplified version of printing whether condition is true or false
+    }
+
 }

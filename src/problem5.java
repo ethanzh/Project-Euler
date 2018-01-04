957a530114ab50feb4964b92186d4fb7967b8301
Ethan Houston <ethan.houston@gmail.com>
2018-01-03 16:40:38 -0800

    Done with 5

--- src/problem5.java
+++ src/problem5.java
@@ -2,7 +2,36 @@ public class problem5 {
 
 
 
+    public static void main(String[] args){
 
+        int startingNumber = 2520;
+
+        while(!evenlyDivisibleOneToX(startingNumber, 20)){
+
+            startingNumber++;
+
+
+        }
+
+        System.out.print(startingNumber);
+
+
+    }
+
+
+
+    static boolean evenlyDivisibleOneToX(int number, int x){
+
+        for(int i = 1; i <= x; i++){
+
+            if (number % i != 0){
+                return false;
+            }
+        }
+
+        return true;
+
+    }
 
 
 }

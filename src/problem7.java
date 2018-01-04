ee5bb93a69a994cf7c80a54a309347403cf70b65
Ethan Houston <ethan.houston@gmail.com>
2018-01-03 17:19:53 -0800

    Finished 7

--- src/problem7.java
+++ src/problem7.java
@@ -2,12 +2,48 @@ public class problem7 {
 
     public static void main(String[] args){
 
+        int i = 1;
+        int currentNumber = 2;
 
+        while (i <= 10001){
+
+            if (prime(currentNumber)){
+
+                System.out.println("Prime #" + i + ": " + currentNumber);
+
+                if (i == 10001){
+                    System.out.println(currentNumber);
+                    break;
+                } else{
+                    i++;
+                    currentNumber++;
+                }
+
+            }else{
+                currentNumber++;
+            }
+
+
+
+        }
+
+        System.out.println(prime(7));
 
     }
 
 
+    static boolean prime(int number){
 
+        for(int i = 2; i <= (number / 2); i++){
+
+            if (number % i == 0){
+                return false;
+
+            }
+        }
+        return true;
+
+    }
 
 
 }

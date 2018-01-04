f978b59b967e1a145943e1ae9ffd98b1fb3a985a
Ethan Houston <ethan.houston@gmail.com>
2018-01-03 16:26:35 -0800

    Palindrome done

--- src/problem4.java
+++ src/problem4.java
@@ -1,10 +1,11 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class problem4 {
 
 
     public static void main(String[] args){
 
-
-        palindrome(12345);
 
 
 
@@ -13,30 +14,23 @@ public class problem4 {
 
     static boolean palindrome(int number){
 
-        int length = String.valueOf(number).length() - 1;
-        int[] reversed = new int[length + 1];
+        int length = String.valueOf(number).length();
 
         String numberstring = String.valueOf(number);
 
-        for (int i = 0; i <= length; i++){
+        String[] reversed = new String[length];
 
-            //reversed[length - (i * 1)] = numberstring.charAt(i);
+        for(int i = 0; i < length; i++){
+
+            reversed[(length - 1) - i] = String.valueOf(numberstring.charAt(i));
 
         }
 
-        reversed[4] = numberstring.charAt(0);
-        System.out.println(reversed[4]);
-        reversed[3] = numberstring.charAt(1);
-        reversed[2] = numberstring.charAt(2);
-        reversed[1] = numberstring.charAt(3);
-        reversed[0] = numberstring.charAt(4);
+        String reversedstring = String.join("", reversed);
 
-        for (int j = 0; j <= length; j++){
-            System.out.println(reversed[j]);
-        }
+        int reversedint = Integer.valueOf(reversedstring);
 
-
-        return false;
+        return number == reversedint;
     }
 
 
